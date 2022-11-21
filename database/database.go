b289330c1342a853654e4769344aa643baa7c3da
Eric <93eric@gmail.com>
2022-11-21 09:55:42 +0100

    Everything working

--- database/database.go
+++ database/database.go
@@ -1,7 +1,10 @@
 package database
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
 	"example.com/crud-user/logs"
@@ -11,7 +14,7 @@ type User struct {
 	gorm.Model
 	FirstName string
 	LastName  string
-	Email     string
+	Email     string `gorm:"primaryKey"`
 }
 
 var (
@@ -20,16 +23,16 @@ var (
 
 func InitDatabase() {
 	var err error
-	/* 	dbUser := os.Getenv("DB_USERNAME")
-	   	dbPass := os.Getenv("DB_PASSWORD")
-	   	dbHost := os.Getenv("DB_HOST")
-	   	dbName := os.Getenv("DB_NAME") */
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
 
-	//dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
-	//DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	DBConn, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//DBConn, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
-		logs.Error.Panic("Failed to connect database")
+		logs.Error.Println("Failed to connect database")
 	}
 	logs.Info.Println("Database connection successfully opened")
 	DBConn.AutoMigrate(&User{})
